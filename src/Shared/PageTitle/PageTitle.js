fc8a7e92bd27dc2a32b58c22282fe2c09f8d9609
Rana Arju <arjurana530@gmail.com>
2022-05-20 18:56:14 +0600

    page title dynamic with Helmet package

--- src/Shared/PageTitle/PageTitle.js
+++ src/Shared/PageTitle/PageTitle.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet'
 
 const PageTitle = ({title}) => {
-    return <title title={title}></title>
+   return (
+      <>
+        <Helmet>
+          <title>{ title }</title>
+        </Helmet>
+       
+      </>
+   )
 };
 
 export default PageTitle;
