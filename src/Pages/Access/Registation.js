052c6209f6fd6ca0dc4b7bed66057b38abaedcd7
Rana Arju <arjurana530@gmail.com>
2022-05-20 17:47:45 +0600

    all necessary package install and some page & components create

---
+++ src/Pages/Access/Registation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registation = () => {
+    return (
+        <div>
+            <h2>Registation</h2>
+        </div>
+    );
+};
+
+export default Registation;
---
+++ src/Pages/Access/SocialMedia.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialMedia = () => {
+    return (
+        <div>
+            <h2>Socail</h2>
+        </div>
+    );
+};
+
+export default SocialMedia;
