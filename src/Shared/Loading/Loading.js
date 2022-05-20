575f501fdbd58f4ee6fd7a83bcc264b344e9dcc0
Rana Arju <arjurana530@gmail.com>
2022-05-20 21:21:22 +0600

    loading spinner design and add access component

---
+++ src/Shared/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+      <div className='h-fit flex justify-center items-center'>
+            <div className="w-16 h-16 border-b-2 border-primary rounded-full animate-spin"></div>
+        </div>
+    );
+};
+
+export default Loading;
